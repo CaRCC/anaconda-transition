c2939368120c29e6647fb71c06ededfe3144f116
StephenSzwiec <Stephen@Szwiec.xyz>
2024-11-15 08:33:31 -0600

    Updated install_miniforge.sh
    - improved granularity of options for miniforge
    - uses provided url to prevent pinning any particular version

--- scripts/install_miniforge.sh
+++ scripts/install_miniforge.sh
@@ -14,34 +14,35 @@
 #    - Work on a variety of shells (toward POSIX compliance)
 #  
 # Arguments: 
-#    - `-a` (optional) sets the alias of mamba as conda
+#    - `-i` (optional) initialize conda for the user's current shell
+#    - `-m` (optional) enable mamba support for faster package management
 #    - `-p` (optional, path) set the prefix for install 
 
 
 # Defaults
 INSTALL_PREFIX="${HOME}/.local/miniforge"
-ALIAS_CONDA=false
-MAMBA_URL="https://github.com/conda-forge/miniforge/releases/download/24.7.1-2/Miniforge3-24.7.1-2-Linux-x86_64.sh"
-CHECKSUM_URL="${MAMBA_URL}.sha256"
+INIT_SHELL=false
+MAMBA_SUPPORT=false
+URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 
 # Parse options
-while getopts "ap:" opt; do
+while getopts "mp:" opt; do
   case $opt in
-    a) ALIAS_CONDA=true ;;
+    i) INIT_SHELL=true ;;
+    m) MAMBA_SUPPORT=true ;;
     p) INSTALL_PREFIX="$OPTARG" ;;
     *) echo "Usage: $0 [-a] [-p install_prefix]" >&2; exit 1 ;;
   esac
 done
 
-# Semi-resilient download function
-# Replace with Aria2 if wanted
 download_file() {
   url="$1"
-  output="$2"
   retries=3
-
   while [ $retries -gt 0 ]; do
-    curl -o "$output" -L "$url" && return 0
+    curl -OL $url 
+    if [ $? -eq 0 ]; then
+      return
+    fi
     retries=$((retries - 1))
     echo "Retrying download ($retries attempts left)..."
     sleep 1
@@ -51,27 +52,25 @@ download_file() {
 }
 
 # Download installer and checksum
-download_file "$MAMBA_URL" "Miniforge3.sh"
-download_file "$CHECKSUM_URL" "Miniforge3.sh.sha256"
-
-# Verify checksum
-if ! sha256sum --check Miniforge3.sh.sha256; then
-  echo "Checksum verification failed. Exiting." >&2
-  exit 1
-fi
+download_file "$URL" 
 
 # Install Miniforge
-chmod u+x Miniforge3.sh
-./Miniforge3.sh -b -p "$INSTALL_PREFIX"
-rm -f Miniforge3.sh Miniforge3.sh.sha256
-
-# Initialize mamba for the user's current shell
-"${INSTALL_PREFIX}/bin/mamba" init --shell "$(basename "$SHELL")"
-
-# Optionally alias conda to mamba
-if [ "$ALIAS_CONDA" = true ]; then
-  echo "alias conda='mamba'" >> "$HOME/.${SHELL##*/}rc"
+chmod u+x $(basename "$URL")
+$SHELL $(basename "$URL") -b -p "$INSTALL_PREFIX"
+if [ $? -ne 0 ]; then
+  echo "Installation failed" >&2
+  exit 1
 fi
+# Initialize conda for the user's current shell
+source "$INSTALL_PREFIX/etc/profile.d/conda.sh"
+# mamba support
+if [ $MAMBA_SUPPORT ]; then
+  source "$INSTALL_PREFIX/etc/profile.d/mamba.sh"
+fi
+if [ $INIT_SHELL ]; then
+  conda init "$(basename $SHELL)"
+fi
+
 
 # Configure conda-forge as the only channel
 # Denylist Anaconda repos
